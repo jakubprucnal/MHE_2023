fbdd0863e8dc827fa98988dd1186b8f67440ec86
matrjx <jakub.prucnal@gmail.com>
2023-03-13 19:05:35 +0100

    Brute force corrected

--- main.cpp
+++ main.cpp
@@ -19,10 +19,9 @@ public:
         for (int i = 0; i < size(); i++) {
             auto &p = *problem;
             auto &t = *this;
-            if (t[i]){
+            if (t[i]) {
                 sums_difference += p[i];
-            }
-            else {
+            } else {
                 sums_difference -= p[i];
             }
         }
@@ -38,6 +37,7 @@ std::ostream &operator<<(std::ostream &o, const problem_t v) {
     o << "}";
     return o;
 }
+
 std::ostream &operator<<(std::ostream &o, const solution_t v) {
     o << "{ ";
     for (auto e: v) o << e << " ";
@@ -49,13 +49,12 @@ std::ostream &operator<<(std::ostream &o, const solution_t v) {
 solution_t brute_force(solution_t start_point) {
     auto solution = start_point;
     for (int i = 0; i < solution.size(); i++) {
-        solution[i] = false;
+        solution[i] = true;
     }
     auto best_solution = solution;
     int i = 0;
-    std::cout << i << " Last: " << solution << std::endl;
-    for (int j = 0; j < solution.size()/2; j++){
-        solution[0] = !solution[0];
+    for (int j = 0; j < solution.size() / 2; j++) {
+        solution[j] = 0;
         do {
             if (solution.goal() <= best_solution.goal()) {
                 best_solution = solution;
@@ -63,12 +62,9 @@ solution_t brute_force(solution_t start_point) {
                           << best_solution.goal() << std::endl;
             }
         } while (std::next_permutation(solution.begin(), solution.end()));
-        std::cout << i << " Last j: " << j << " "<< solution << std::endl;
     }
-
     return best_solution;
 }
-
 
 
 int main() {
@@ -76,10 +72,18 @@ int main() {
     problem_t part_problem = {
             1, 3, 5, 7, 9, 5, 1, 1, 1
     };
-    cout << part_problem << std::endl;
+//    cout << part_problem << std::endl;
     solution_t solution(make_shared<problem_t>(part_problem));
     for (int i = 0; i < part_problem.size(); i++) solution.push_back(true);
     solution = brute_force(solution);
-    cout << solution << "Result  " << solution.goal() << std::endl;
+    cout << solution << " Result  " << solution.goal() << std::endl;
+//    int j = 0;
+//    for (int i = 0; i < solution.size()/2; i++){
+//        solution[i] = 0;
+//        do {
+//            std::cout << (j++) << " " << solution << std::endl;
+//        } while (std::next_permutation(solution.begin(), solution.end()));
+//        std::cout << "Last: " << " "<< solution << std::endl;
+//    }
 
 }
